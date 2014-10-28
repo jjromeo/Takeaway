3d822d8898f38449d5ed1e611d76c0562439f00e
Jerome Pratt <jerome.pratt@outlook.com>
2014-10-28 15:51:43 +0000

    added Takeaway#make_order method which is insanely clunky but passes test - must refactor

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -5,8 +5,23 @@ class Takeaway
        @dishes = OpenStruct.new(quarter_chicken: 2.5, half_chicken: 5, whole_chicken: 10, chicken_platter: 15)
    end
    
-   def make_order(dishes = {}, total)
-
+   def make_order(orders = {}, total)
+       quarter_chicken_count = orders[:quarter_chicken]
+       half_chicken_count = orders[:half_chicken]
+       whole_chicken_count = orders[:whole_chicken]
+       chicken_platter_count = orders[:chicken_platter]
+       quarter_chicken_total = quarter_chicken_count * dishes.quarter_chicken if quarter_chicken_count
+       half_chicken_total = half_chicken_count * dishes.half_chicken if half_chicken_count
+       whole_chicken_total = whole_chicken_count * dishes.whole_chicken if whole_chicken_count
+       chicken_platter_total = chicken_platter_count * dishes.chicken_platter if chicken_platter_count
+       totals_array = [quarter_chicken_total, half_chicken_total, whole_chicken_total, chicken_platter_total]
+       existing_totals = totals_array.reject {|total| total.nil?}
+       total_total = existing_totals.inject(:+)
+       if total_total == total
+           "order correct and being processed"
+       else
+           raise "The total amount is wrong! please try again"
+       end
    end
 end
 
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -6,12 +6,13 @@ describe Takeaway do
         expect(nandos.dishes.half_chicken).to eq 5
         expect(nandos.dishes.whole_chicken).to eq 10
         expect(nandos.dishes.chicken_platter).to eq 15
-        
     end
 
     it 'should allow someone to order a dish by giving list, quantity and total' do
         nandos = Takeaway.new
-        expect{ nandos.make_order({dishes:{quarter_chicken: 1, 
-                                          half_chicken: 2}}, 15)}.not_to raise_error
+        expect{ nandos.make_order({quarter_chicken: 2, 
+                                          half_chicken: 2}, 15)}.not_to raise_error
+        expect(nandos.make_order({quarter_chicken: 2, 
+                                          half_chicken: 2}, 15)).to eq "order correct and being processed"
     end
 end
