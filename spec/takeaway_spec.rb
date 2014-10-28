2447bd2784319d9d64341391d80f6cb1f6534b74
Jerome Pratt <jerome.pratt@outlook.com>
2014-10-28 11:43:04 +0000

    added other dishes to the test

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -2,7 +2,11 @@ describe Takeaway do
     
     it 'should initialize with a list of dishes with prices' do
         nandos = Takeaway.new
+        expect(nandos.dishes.quarter_chicken).to eq 2.5
         expect(nandos.dishes.half_chicken).to eq 5
+        expect(nandos.dishes.whole_chicken).to eq 10
+        expect(nandos.dishes.chicken_platter).to eq 15
+        
     end
 
 end
