f99a6191765ba0c35a27d2f84858481b913ee629
Jerome Pratt <jerome.pratt@outlook.com>
2014-10-28 17:13:41 +0000

    refactored method

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -6,18 +6,27 @@ class Takeaway
    end
    
    def make_order(orders = {}, total)
-       quarter_chicken_count = orders[:quarter_chicken]
-       half_chicken_count = orders[:half_chicken]
-       whole_chicken_count = orders[:whole_chicken]
-       chicken_platter_count = orders[:chicken_platter]
-       quarter_chicken_total = quarter_chicken_count * dishes.quarter_chicken if quarter_chicken_count
-       half_chicken_total = half_chicken_count * dishes.half_chicken if half_chicken_count
-       whole_chicken_total = whole_chicken_count * dishes.whole_chicken if whole_chicken_count
-       chicken_platter_total = chicken_platter_count * dishes.chicken_platter if chicken_platter_count
-       totals_array = [quarter_chicken_total, half_chicken_total, whole_chicken_total, chicken_platter_total]
+       @quarter_chicken_count = orders[:quarter_chicken]
+       @half_chicken_count = orders[:half_chicken]
+       @whole_chicken_count = orders[:whole_chicken]
+       @chicken_platter_count = orders[:chicken_platter]
+       @total = total
+       set_totals
+       get_total
+   end
+   
+   def set_totals
+       @quarter_chicken_total = @quarter_chicken_count * dishes.quarter_chicken if @quarter_chicken_count
+       @half_chicken_total = @half_chicken_count * dishes.half_chicken if @half_chicken_count
+       @whole_chicken_total = @whole_chicken_count * dishes.whole_chicken if @whole_chicken_count
+       @chicken_platter_total = @chicken_platter_count * dishes.chicken_platter if @chicken_platter_count
+   end
+
+   def get_total
+       totals_array = [@quarter_chicken_total, @half_chicken_total, @whole_chicken_total, @chicken_platter_total]
        existing_totals = totals_array.reject {|total| total.nil?}
        total_total = existing_totals.inject(:+)
-       if total_total == total
+       if total_total == @total
            "order correct and being processed"
        else
            raise "The total amount is wrong! please try again"
