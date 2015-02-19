c9b7af2112f461ca1209a39b98c1262a3661e629
Jerome Pratt <jerome.pratt@outlook.com>
2015-02-19 18:57:28 +0000

    changing code-styling

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -3,53 +3,53 @@ require 'twilio-ruby'
 require './secrets.rb'
 
 class Takeaway
-    include SecretsHelper
-    attr_accessor :dishes
-   def initialize
-       @dishes = OpenStruct.new(quarter_chicken: 2.5,
-                                half_chicken: 5,
-                                whole_chicken: 10,
-                                chicken_platter: 15)
-       set_secrets
-   end
-   
-   def make_order(orders = {}, total)
-       @quarter_chicken_count = orders[:quarter_chicken]
-       @half_chicken_count = orders[:half_chicken]
-       @whole_chicken_count = orders[:whole_chicken]
-       @chicken_platter_count = orders[:chicken_platter]
-       @total = total
-       set_totals
-       get_total
-   end
-   
-   def set_totals
-       @quarter_chicken_total = @quarter_chicken_count * dishes.quarter_chicken if @quarter_chicken_count
-       @half_chicken_total = @half_chicken_count * dishes.half_chicken if @half_chicken_count
-       @whole_chicken_total = @whole_chicken_count * dishes.whole_chicken if @whole_chicken_count
-       @chicken_platter_total = @chicken_platter_count * dishes.chicken_platter if @chicken_platter_count
-   end
+  include SecretsHelper
+  attr_accessor :dishes
+  def initialize
+    @dishes = OpenStruct.new(quarter_chicken: 2.5,
+                             half_chicken: 5,
+                             whole_chicken: 10,
+                             chicken_platter: 15)
+    set_secrets
+  end
 
-   def get_total
-       totals_array = [@quarter_chicken_total, @half_chicken_total, @whole_chicken_total, @chicken_platter_total]
-       existing_totals = totals_array.reject {|total| total.nil?}
-       total_total = existing_totals.inject(:+)
-       if total_total == @total
-           puts "order correct and being processed"
-           send_message
-       else
-           raise "The total amount is wrong! please try again"
-       end
-   end
+  def make_order(orders = {}, total)
+    @quarter_chicken_count = orders[:quarter_chicken]
+    @half_chicken_count = orders[:half_chicken]
+    @whole_chicken_count = orders[:whole_chicken]
+    @chicken_platter_count = orders[:chicken_platter]
+    @total = total
+    set_totals
+    get_total
+  end
 
-   def set_secrets
-       super
-   end
+  def set_totals
+    @quarter_chicken_total = @quarter_chicken_count * dishes.quarter_chicken if @quarter_chicken_count
+    @half_chicken_total = @half_chicken_count * dishes.half_chicken if @half_chicken_count
+    @whole_chicken_total = @whole_chicken_count * dishes.whole_chicken if @whole_chicken_count
+    @chicken_platter_total = @chicken_platter_count * dishes.chicken_platter if @chicken_platter_count
+  end
 
-    def send_message
-    @message = client.account.messages.create({to: "+447903524834",
-                                              from: "+441683292035",
-                                              body: "We are processing your order, it will arrive soon!"})
+  def get_total
+    totals_array = [@quarter_chicken_total, @half_chicken_total, @whole_chicken_total, @chicken_platter_total]
+    existing_totals = totals_array.reject {|total| total.nil?}
+    total_total = existing_totals.inject(:+)
+    if total_total == @total
+      puts "order correct and being processed"
+      send_message
+    else
+      raise "The total amount is wrong! please try again"
     end
+  end
+
+  def set_secrets
+    super
+  end
+
+  def send_message
+    @message = client.account.messages.create({ to: "+447903524834",
+                                               from: "+441683292035",
+                                               body: "We are processing your order, it will arrive soon!" })
+  end
 end
 
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,27 +1,27 @@
-describe Takeaway do 
+describe Takeaway do
 
-    let(:nandos) {Takeaway.new}
-    
-        it 'should initialize with a list of dishes with prices' do
-            expect(nandos.dishes.quarter_chicken).to eq 2.5
-            expect(nandos.dishes.half_chicken).to eq 5
-            expect(nandos.dishes.whole_chicken).to eq 10
-            expect(nandos.dishes.chicken_platter).to eq 15
-        end
+  let(:nandos) {Takeaway.new}
 
-        it 'should allow someone to order a dish by giving list, quantity and total and that to send a Twilio message' do
-            VCR.use_cassette('twilio') do
-                expect{ nandos.make_order({quarter_chicken: 2, 
-                                                  half_chicken: 2}, 15)}.not_to raise_error
-                expect(nandos.make_order({quarter_chicken: 2, half_chicken: 2}, 15)).to be_an_instance_of Twilio::REST::Message 
-            end
+  it 'should initialize with a list of dishes with prices' do
+    expect(nandos.dishes.quarter_chicken).to eq 2.5
+    expect(nandos.dishes.half_chicken).to eq 5
+    expect(nandos.dishes.whole_chicken).to eq 10
+    expect(nandos.dishes.chicken_platter).to eq 15
+  end
+
+  it 'should allow someone to order a dish by giving list, quantity and total and that to send a Twilio message' do
+    VCR.use_cassette('twilio') do
+      expect{ nandos.make_order({ quarter_chicken: 2,
+                                 half_chicken: 2 }, 15) }.not_to raise_error
+      expect(nandos.make_order({ quarter_chicken: 2, half_chicken: 2 }, 15)).to be_an_instance_of Twilio::REST::Message
     end
+  end
 
-        it 'should not let someone order a dish if they get the sum incorrect' do  
-         expect{ nandos.make_order({quarter_chicken: 2, 
-                                          half_chicken: 2}, 25)}.to raise_error "The total amount is wrong! please try again"
-    end
+  it 'should not let someone order a dish if they get the sum incorrect' do
+    expect{ nandos.make_order({ quarter_chicken: 2,
+                               half_chicken: 2 }, 25) }.to raise_error "The total amount is wrong! please try again"
+  end
 
-        it 'should activate twilio\'s message sending when a successful order is made'   do 
-    end
+  it 'should activate twilio\'s message sending when a successful order is made'   do
+  end
 end
